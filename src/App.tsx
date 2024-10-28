546a79025a46379c38088d27d415de0a043242d5
Alex <phoenix.trade2307@gmail.com>
2024-10-28 23:16:25 +0200

    added navlinks and navigate

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css'
 import {PageOne} from "./components/pages/PageOne";
 import {PageTwo} from "./components/pages/PageTwo";
 import {PageThree} from "./components/pages/PageThree";
-import {Route, Routes} from "react-router-dom";
+import {Link, Navigate, Route, Routes} from "react-router-dom";
 import {Error404} from "./components/pages/Error404";
 
 
@@ -13,15 +13,27 @@ function App() {
         <div className={'./App'}>
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
+{/*                <div className={styles.nav}>
+                    <Link to={'/page1'}> ADIDAS</Link>
+                </div>
                 <div className={styles.nav}>
-                    Здесь будет навигация
+                    <Link to={'/page2'}> PUMA</Link>
+                </div>
+                <div className={styles.nav}>
+                    <Link to={'/page3'}> Abibas</Link>
+                </div>*/}
+                <div className={styles.nav}>
+                    <Link to={'/page1'}> ADIDAS</Link>
+                    <Link to={'/page2'}> PUMA</Link>
+                    <Link to={'/page3'}> Abibas</Link>
                 </div>
                 <div className={styles.content}>
                     <Routes>
                         <Route path={'/page1'} element={<PageOne/>}/>
                         <Route path={'/page2'} element={<PageTwo/>}/>
                         <Route path={'/page3'} element={<PageThree/>}/>
-                        <Route path={'*'} element={<Error404/>}/>
+                        <Route path={'/error404'} element={<Error404/>}/>
+                        <Route path={'/*'} element={<Navigate to={'/error404'}/>}/>
                     </Routes>
                 </div>
             </div>
