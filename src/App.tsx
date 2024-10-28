d5739a5963f44b2ab00fbf79c5735db4b8bf9822
Alex <phoenix.trade2307@gmail.com>
2024-10-28 22:10:14 +0200

    connection routes and error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styles from "./components/Site.module.css";
+import './App.css'
+import {PageOne} from "./components/pages/PageOne";
+import {PageTwo} from "./components/pages/PageTwo";
+import {PageThree} from "./components/pages/PageThree";
+import {Route, Routes} from "react-router-dom";
+import {Error404} from "./components/pages/Error404";
 
 
 function App() {
     return (
-        <div>
+        <div className={'./App'}>
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
                     Здесь будет навигация
                 </div>
                 <div className={styles.content}>
-                    Здесь будут кроссовки
+                    <Routes>
+                        <Route path={'/page1'} element={<PageOne/>}/>
+                        <Route path={'/page2'} element={<PageTwo/>}/>
+                        <Route path={'/page3'} element={<PageThree/>}/>
+                        <Route path={'*'} element={<Error404/>}/>
+                    </Routes>
                 </div>
             </div>
             <div className={styles.footer}>abibas 2023</div>
