63adaa9c82635812f9d6729045f4b1fc38d87f8f
Alex <phoenix.trade2307@gmail.com>
2025-01-04 14:41:30 +0200

    added search

--- src/nepomniaschiy/pages/Posts.tsx
+++ src/nepomniaschiy/pages/Posts.tsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from "react";
-import {Link} from "react-router-dom";
+import {Link, useSearchParams} from "react-router-dom";
 import {PATH} from "../app/New_App";
 
 export type Post = {
@@ -10,6 +10,17 @@ export type Post = {
 }
 export const Posts = () => {
     const [posts, setPosts] = useState<Post[]>([])
+    const [searchParams, setSearchParams] = useSearchParams()
+    const postQuery = searchParams.get('post') || ''
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault()
+        const form = event.target
+        const query = form.search.value
+
+        setSearchParams({post: query})
+    }
+
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(res => res.json())
@@ -18,18 +29,25 @@ export const Posts = () => {
     return (
         <div>
             <h3>POSTS</h3>
+            <form autoComplete={'off'} onSubmit={handleSubmit}>
+                <input type="search" name={'search'}/>
+                <input type="submit" name={'Search'}/>
+            </form>
+
             <Link to={PATH.NEW_POST} style={{color: 'cadetblue'}}>Add new post</Link>
             {posts.length > 0
-                ? posts.map(post => {
-                    return (
-                        <>
-                            <Link key={post.id} to={`/posts/${post.id}`}
-                                  style={{color: 'white'}}>
-                                <h4>{post.title}</h4>
-                            </Link>
-                        </>
-                    )
-                })
+                ? posts
+                    .filter(post => post.title.includes(postQuery))
+                    .map(post => {
+                        return (
+                            <>
+                                <Link key={post.id} to={`/posts/${post.id}`}
+                                      style={{color: 'white'}}>
+                                    <h4>{post.title}</h4>
+                                </Link>
+                            </>
+                        )
+                    })
                 : (
                     <p>No posts</p>
                 )
