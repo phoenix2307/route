8aedd67ccfe1dadfcc289e421768110dcf0ff197
Alex <phoenix.trade2307@gmail.com>
2025-01-04 14:19:15 +0200

    realisation logIn and logOut

--- src/nepomniaschiy/pages/CreatePost.tsx
+++ src/nepomniaschiy/pages/CreatePost.tsx
@@ -1,7 +1,17 @@
+import {useNavigate} from "react-router-dom";
+import {useAuth} from "../hook/useAuth";
+
 export const CreatePost = () => {
+    const {signOut} = useAuth()
+
+    const navigate = useNavigate()
+    const handleSignOut = () => {
+        signOut(() => navigate('/', {replace: true}))
+    }
     return (
         <div>
             <h2>Create a new post</h2>
+            <button onClick={handleSignOut}>Log out</button>
         </div>
     )
 }
--- src/nepomniaschiy/pages/LoginPage.tsx
+++ src/nepomniaschiy/pages/LoginPage.tsx
@@ -1,14 +1,28 @@
 import {useLocation, useNavigate} from "react-router-dom";
+import {useAuth} from "../hook/useAuth";
 
 export const LoginPage = () => {
     const navigate = useNavigate()
     const location = useLocation()
+    const {signIn} = useAuth()
 
     const fromPage = location.state?.from?.pathname || '/'
+    const handleSubmit = (event: any) => {
+        event.preventDefault()
+        const form = event.target
+        const user = form.username.value
+
+        signIn(user, () => navigate(fromPage, {replace: true}))
+    }
     return (
         <div>
             <h1>Login page</h1>
-            {fromPage}
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name: <input name={'username'}/>
+                </label>
+                <button type={'submit'}>log in</button>
+            </form>
         </div>
     )
 }
